02259341b387d17208048e614312d1df37445b02
coder-zim <c0d3r.zim@gmail.com>
2025-06-30 15:44:17 -0400

    trench0r_bot import!

--- telegram_bot.py
+++ telegram_bot.py
@@ -70,4 +70,5 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(main())
->>>>>>> 7ab909b (trench0r_bot CPR!! He's.. ALIVE!!!)
+
+# (trench0r_bot CPR!! He's.. ALIVE!!!)
